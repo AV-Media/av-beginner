383b7b43352e3b8de56d6b3ca62e2bc1fc4e4c1d
glumes <zhaoying9402@gmail.com>
2020-03-01 19:59:53 +0800

    av-beginner: create sdl window and display with special color

--- main.cpp
+++ main.cpp
@@ -18,11 +18,43 @@
 #include <SDL2/SDL.h>
 using namespace std;
 
-int main(){
+int main() {
 
-    cout << "hello av-beginner" << endl;
+    if (SDL_Init(SDL_INIT_EVERYTHING) < 0){
+        return -1;
+    }
 
-    SDL_Init(SDL_INIT_EVERYTHING);
+    int width = 400;
+    int height = 400;
+    SDL_Window *window = SDL_CreateWindow("Hello SDL",
+            SDL_WINDOWPOS_CENTERED,
+            SDL_WINDOWPOS_CENTERED,
+            width,height,
+            SDL_WINDOW_ALLOW_HIGHDPI);
+
+
+    SDL_Renderer* pRenderer = NULL;
+    // 创建渲染器
+    pRenderer = SDL_CreateRenderer(window, -1, 0);
+    // 指定渲染颜色
+    SDL_SetRenderDrawColor(pRenderer,0,255,0,255);
+    // 清空当前窗口的颜色
+    SDL_RenderClear(pRenderer);
+    // 执行渲染操作，更新窗口
+    SDL_RenderPresent(pRenderer);
+
+    SDL_Event windowEvent;
+    while (true){
+        if (SDL_PollEvent(&windowEvent)){
+            if (SDL_QUIT == windowEvent.type){
+                break;
+            }
+        }
+    }
+
+    SDL_DestroyWindow(window);
+    SDL_DestroyRenderer(pRenderer);
+    SDL_Quit();
 
     return 0;
 }
