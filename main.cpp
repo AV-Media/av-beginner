2fb562f3503bdce591b4984923bf024ded04d37a
glumes <zhaoying9402@gmail.com>
2020-03-03 14:45:09 +0800

    av-beginner: sdl event usage

--- main.cpp
+++ main.cpp
@@ -18,6 +18,18 @@
 #include <SDL2/SDL.h>
 using namespace std;
 
+#define SDL_CUSTOM_EVENT  (SDL_USEREVENT + 1)
+
+int sdl_thread_custom_event(void *){
+
+    SDL_Delay(5000);
+
+    SDL_Event sdlEvent;
+
+    sdlEvent.type = SDL_CUSTOM_EVENT;
+    SDL_PushEvent(&sdlEvent);
+}
+
 int main() {
 
     if (SDL_Init(SDL_INIT_EVERYTHING) < 0){
@@ -44,13 +56,53 @@ int main() {
     SDL_RenderPresent(pRenderer);
 
     SDL_Event windowEvent;
-    while (true){
-        if (SDL_PollEvent(&windowEvent)){
-            if (SDL_QUIT == windowEvent.type){
-                break;
+
+//    while (true){
+//        if (SDL_PollEvent(&windowEvent)){
+//            if (SDL_QUIT == windowEvent.type){
+//                break;
+//            }
+//        }
+//    }
+
+    SDL_CreateThread(sdl_thread_custom_event, "custom_event", nullptr);
+
+
+    bool bQuit = false;
+    while (!bQuit){
+        while (SDL_PollEvent(&windowEvent)){
+            switch (windowEvent.type){
+                case SDL_QUIT:
+                    bQuit = true;
+                    break;
+                case SDL_KEYDOWN:
+                    if (windowEvent.key.keysym.sym == SDLK_SPACE){
+                        cout << "user click space \n" ;
+                    }
+                    break;
+                case SDL_MOUSEBUTTONDOWN:
+                    printf("button index  is %d\n",windowEvent.button.button);
+                    break;
+                case SDL_CUSTOM_EVENT:
+                    cout << "receive user custom event\n";
+                    break;
+                default:
+                    break;
             }
         }
     }
+
+//    通过 SDL_WaitEvent 来监听事件
+//    bool bQuit = false;
+//    while (!bQuit){
+//        SDL_WaitEvent(&windowEvent);
+//        if (windowEvent.type == SDL_QUIT){
+//            bQuit = true;
+//            break;
+//        } else{
+//            cout << "get event" << endl;
+//        }
+//    }
 
     SDL_DestroyWindow(window);
     SDL_DestroyRenderer(pRenderer);
